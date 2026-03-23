2e3762de4a38b42ed58038da09da38830780e5a3
shreyash315 <shreyashgaikwad315@gmail.com>
2026-03-23 19:02:11 +0530

    Login As and forgot Password function added

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function flipCard(){
 document.getElementById("card").classList.toggle("flip");
 }
+function toggleDropdown() {
+    document.getElementById("dropdownOptions").classList.toggle("show");
+}
+
+function selectRole(role, icon) {
+    document.getElementById("selectedRole").innerText = "Login as " + role;
+
+    document.querySelector(".selected i").className = "fa-solid " + icon;
+
+    document.getElementById("dropdownOptions").classList.remove("show");
+}
+function openForgot() {
+    document.getElementById("forgotModal").style.display = "flex";
+}
+
+function closeForgot() {
+    document.getElementById("forgotModal").style.display = "none";
+}
